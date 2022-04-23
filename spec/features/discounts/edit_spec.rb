b941887b371233b2ceaaa2c5f6455bdc39d7f2e5
Sandisz Thieme <sandiszthieme@gmail.com>
2022-04-22 21:31:06 -0600

    Test: Discount edit page has form to edit the discount

--- spec/features/discounts/edit_spec.rb
+++ spec/features/discounts/edit_spec.rb
@@ -1,43 +1,47 @@
 require "rails_helper"
 
-describe "Discount New Page" do
+describe "Discount Edit Page" do
   before do
     @merchant = Merchant.create!(name: "Store Store")
+    @m_discount = @merchant.bulk_discounts.create!( percentage: 20, quantity_threshold: 10)
   end
 
-  it "has a form to create a new merchant" do
+  it "has a form to edit the current merchant" do
     visit "/merchants/#{@merchant.id}/discounts"
-    expect(page).to_not have_content("Discount: buy 10 get 50% off")
+    expect(page).to have_content("Discount: buy 10 get 20% off")
 
-    click_link("New discount")
+    within "#discount-#{@m_discount.id}" do
+      click_button("Edit this discount")
+    end
 
     fill_in('Quantity threshold', with: '10')
     fill_in('Percentage', with: '50')
     click_button("Save")
-
     expect(current_path).to eq("/merchants/#{@merchant.id}/discounts")
+
+    expect(page).not_to have_content("Discount: buy 10 get 20% off")
     expect(page).to have_content("Discount: buy 10 get 50% off")
   end
 
 
   it "displays error message if info is not input correctly" do
-    visit "/merchants/#{@merchant.id}/discounts/new"
+    visit "/discounts/#{@m_discount.id}/edit"
 
     fill_in('Quantity threshold', with: '10')
     fill_in('Percentage', with: '300')
     click_button("Save")
 
-    expect(current_path).to eq("/merchants/#{@merchant.id}/discounts/new")
+    expect(current_path).to eq("/discounts/#{@m_discount.id}/edit")
     expect(page).to have_content("Error: • quantity threshold and percentage must not be blank • percentage must be between 0 and 100")
   end
 
   it "displays error message if info is not input correctly" do
-    visit "/merchants/#{@merchant.id}/discounts/new"
+    visit "/discounts/#{@m_discount.id}/edit"
 
-    fill_in('Quantity threshold', with: '10')
+    fill_in('Quantity threshold', with: '')
     click_button("Save")
 
-    expect(current_path).to eq("/merchants/#{@merchant.id}/discounts/new")
+    expect(current_path).to eq("/discounts/#{@m_discount.id}/edit")
     expect(page).to have_content("Error: • quantity threshold and percentage must not be blank • percentage must be between 0 and 100")
   end
 end
