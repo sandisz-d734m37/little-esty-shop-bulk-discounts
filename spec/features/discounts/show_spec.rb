0a6ef2e808a96b55fa0585d9fa35916ba76b7aea
Sandisz Thieme <sandiszthieme@gmail.com>
2022-04-22 18:57:06 -0600

    Add pertinent info to discount show page

--- spec/features/discounts/show_spec.rb
+++ spec/features/discounts/show_spec.rb
@@ -4,14 +4,14 @@ describe "merchant discount index page" do
   before do
     @merchant_1 = Merchant.create!(name: "Store Store")
 
-    @m1_discount1 = @merchant_1.bulk_discounts.create!( percentage: 20, quantity_threshold: 10)
-    @m1_discount2 = @merchant_1.bulk_discounts.create!( percentage: 30, quantity_threshold: 20)
+    @m1_discount1 = @merchant_1.bulk_discounts.create!(quantity_threshold: 10, percentage: 20)
+    @m1_discount2 = @merchant_1.bulk_discounts.create!(quantity_threshold: 20, percentage: 30)
 
     visit "/discounts/#{@m1_discount2.id}"
   end
 
   it 'displays pertinent information' do
-    expect(page).to have_content("With purchase of 30, customer's recieve 20% off")
-    expect(page).not_to have_content("With purchase of 20, customer's recieve 10% off")
+    expect(page).to have_content("With purchase of 20, customer's recieve 30% off")
+    expect(page).not_to have_content("With purchase of 10, customer's recieve 20% off")
   end
 end
